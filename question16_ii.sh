d44a399c761bdbc9fcdbfe4048421eba06ccaa25
NamanVeerwal <107531013+NamanVeerwal@users.noreply.github.com>
2025-06-05 06:34:45 +0000

    Sixteenth

--- question16_ii.sh
+++ question16_ii.sh
@@ -2,4 +2,4 @@ cp -r dir16_1 dir16_1_copy
 rm -r dir16_1_copy/dir16_5
 mv dir16_1/dir16_2/dir16_3/dir16_4 dir16_1/dir16_2
 mv dir16_1/dir16_2/file16_2 dir16_1/dir16_2/file16_2_renamed
-rm -r dir16_1/dir16_5/file16_5
+rm -r dir16_1/dir16_5/file16_4
